cec37d10b0f3a3bd2fce788caa56c3c2a54ea120
Prabhat Kumar <prabhatprajapati21@gmail.com>
2025-01-21 21:08:43 +0530

    vnv

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './components/header'
+import Footer from './components/footer'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Footer />
+    </>
+  )
+}
+
+export default App
